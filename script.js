7207c10540e2d98ecdf09aa8f3d126d441bd07bb
CkgCam <ckgkaukau@gmail.com>
2021-07-30 23:57:40 +1200

    auto play

---
+++ script.js
@@ -0,0 +1,8 @@
+    //select the first iframe that has a src that starts with "//www.youtube"
+    var firstIframe = document.querySelector('iframe[src^="//www.youtube"]');
+    //get the current source
+    var src = firstIframe.src;
+    //update the src with "autoplay=1"
+    var newSrc = src+'?autoplay=1';
+    //change iframe's src
+    firstIframe.src = newSrc;
